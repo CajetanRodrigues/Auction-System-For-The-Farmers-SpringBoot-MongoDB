25ac976fa687b02c78cf6c4d751c8bc239b82127
Unknown <rigrod1999@gmail.com>
2019-03-06 23:53:16 +0530

    updated

--- src/main/java/com/journaldev/bootifulmongodb/controller/GroupController.java
+++ src/main/java/com/journaldev/bootifulmongodb/controller/GroupController.java
@@ -12,7 +12,7 @@ import com.journaldev.bootifulmongodb.dal.GroupRepository;
 import com.journaldev.bootifulmongodb.dal.UserRepository;
 import com.journaldev.bootifulmongodb.model.Group;
 import com.journaldev.bootifulmongodb.model.User;
-
+//edited
 @RestController
 @RequestMapping(value="/rest")
 public class GroupController {
